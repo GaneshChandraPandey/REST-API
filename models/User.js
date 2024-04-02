f745200fecae9388f3d03c5fc6061f6f9eb3af4b
ganesh <gpandey059@gmail.com>
2024-04-02 20:26:58 +0530

    add sone new features

--- models/User.js
+++ models/User.js
@@ -35,7 +35,7 @@ const UserSchema =new mongoose.Schema({
         default:[]
       
     },
-    followins:{
+    followings:{
         type:Array,
         default:[]
       
@@ -43,7 +43,25 @@ const UserSchema =new mongoose.Schema({
     isAdmin:{
         type:Boolean,
         default:false,
-    }
+    },
+    desc:{
+        type:String,
+        max:50
+    },
+city:{
+    type:String,
+    max:50
+},
+from:{
+    type:String,
+    max:50
+
+},
+relationship:{
+    type:Number,
+    enum:[1,2,3]
+},
+
 },
 {timestamps:true}
 );
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,50 @@
 const router = require("express").Router();
 const User = require("../models/User")
+const bcrypt = require("bcrypt")
 //REGISTER
-router.get("/register",async (req,res)=>{
-    const user =  await new User ({
-        UserName:"ganesh",
-        email:"gpandey121",
-        password:"123445"
+router.post("/register",async (req,res)=>{
+   
+   try{
+// generate new password
 
-    })
-     await user.save()
-     res.send("OK")
+const salt = await bcrypt.genSalt(10);
+const hashedPassword = await bcrypt.hash(req.body.password ,salt);
+
+// create new user 
+
+const newUser = new User({
+    UserName:req.body.UserName,
+    email:req.body.email,
+    password:hashedPassword
+   });
+
+
+// save user and return
+
+    const user = await newUser.save();
+    res.status(200).json(user);
+   }catch(err){
+    res.status(500).json(err)
+   }
 });
+
+
+//LOGIN
+
+router.post("/login",async (req,res)=>{
+    try{
+    const user = await User.findOne({email: req.body.email});
+    !user && res.status(404).json("user not found")
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400 ).json("wrong password")
+
+    res.status(200).json(user)
+    }catch(err){
+        res.status(500).json(err)
+
+    }
+})
 
 
 
