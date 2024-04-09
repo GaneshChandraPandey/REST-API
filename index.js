2a3a165517625455a0512201d9c79da65f268d5f
ganesh <gpandey059@gmail.com>
2024-04-09 20:01:58 +0530

    new updates

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute =require("./routes/users")
+const postRoute =require("./routes/posts")
 const authRoute =require("./routes/auth")
 
 
@@ -27,7 +28,8 @@ dotenv.config();
     app.use(helmet());
     app.use(morgan("common"));
 
-   app.use("/api/user", userRoute);
+   app.use("/api/users", userRoute);
+   app.use("/api/posts", postRoute);
    app.use("/api/auth", authRoute);
 
 app.listen(8800,()=>{
